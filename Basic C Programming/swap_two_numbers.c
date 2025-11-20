0bae2fa6561a137af2fe2a149a2250000ce8bdd1
Lohesh M <lohesh.m.2024.cse@rajalakshmi.edu.in>
2025-11-20 20:24:00 +0530

    Create swap_two_numbers.c

---
+++ Basic C Programming/swap_two_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+ int a,b;
+ scanf("%d %d",&a,&b);
+ int *ptr1=&a; int *ptr2=&b;
+
+ /* method using pointers swapping two numbers using their address
+ int temp=*ptr1;
+ *ptr1=*ptr2;
+ *ptr2=temp; */
+
+ /* method using x-or operation */
+ *ptr1=*ptr1^*ptr2;
+ *ptr2=*ptr1^*ptr2;
+ *ptr1=*ptr1^*ptr2;
+ /*can also be done suing variables qa and b but done here because the pointers are declared*/
+
+ printf("%d %d\n",
+*ptr1,
+*ptr2);
+
+ return 0;
+}
