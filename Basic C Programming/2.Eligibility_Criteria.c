edc0d7a78d5cb48c4718f71b96f757580c9ae01c
Lohesh M <lohesh.m.2024.cse@rajalakshmi.edu.in>
2025-11-20 20:31:34 +0530

    Update 2.Eligibility_Criteria.c

--- Basic C Programming/2.Eligibility_Criteria.c
+++ Basic C Programming/2.Eligibility_Criteria.c
@@ -1,3 +1,28 @@
+/*
+ Write a C program to find the eligibility of admission for a professional course based on the following criteria:
+ Marks in Maths >= 65
+ Marks in Physics >= 55
+ Marks in Chemistry >= 50
+ Or
+ Total in all three subjects >= 180
+ Sample Test Cases
+ Test Case 1
+ Input
+ 70 60 80
+ Output
+ The candidate is eligible
+ Test Case 2
+ Input
+ 50   80   80
+ Output
+ The candidate is eligible
+ Test Case 3
+ Input
+ 50   60   40
+ Output
+ The candidate is not eligible
+*/
+
 #include<stdio.h>
 int main(){
  int m1,m2,m3;
